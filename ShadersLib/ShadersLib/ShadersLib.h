542eada6ccd611ae80f37126f573e1a114d42e3e
mizquierdo97 <mizquierdo97@gmail.com>
2018-01-19 23:16:02 +0100

    working on shaders creation

--- ShadersLib/ShadersLib/ShadersLib.h
+++ ShadersLib/ShadersLib/ShadersLib.h
@@ -9,12 +9,22 @@
 
 
 #include <string>
+#include <list>
 #include "Glew\include\glew.h"
 
 #pragma comment (lib, "Glew/libx86/glew32.lib") 
 typedef unsigned int uint;
 
 namespace ShaderLib {
+
+	enum ShaderType {
+		nullShader,
+		fragment,
+		geometry,
+		vertex,
+	};
+
+
 	class Shader {
 	public:
 		Shader() {
@@ -23,58 +33,66 @@ namespace ShaderLib {
 		~Shader() {
 
 		}
+	public:
+		uint shaderID = 0;
 
-		SHADERS_API void AddFragment(int ID);
-		SHADERS_API void AddVertex(int ID);
-		SHADERS_API void AddGeometry(int ID);
+		std::string shaderPath = "";
+
+		std::string shaderText = "";
+
+		ShaderType shaderType = ShaderType::nullShader;
+
+	};
+
+
+	class ShaderProgram {
+	public:
+		ShaderProgram() {
+
+		}
+		~ShaderProgram() {
+
+		}
+
+		SHADERS_API void AddFragment(Shader* fragment);
+		SHADERS_API void AddVertex(Shader* vertex);
+		SHADERS_API void AddGeometry(Shader* geometry);
 
 		SHADERS_API bool LoadProgram();
 
 	public:
 		char* name = nullptr;
 
-		int programID = 0;
+		uint programID = 0;
 
-		int fragmentID = 0;
-		int vertexID = 0;
-		int geometryID = 0;
+		Shader* fragment = nullptr;
+		Shader* vertex = nullptr;
+		Shader* geometry = nullptr;
 
-		std::string fragment_path;
-		std::string vertex_path;
-		std::string geometry_path;
-
-
-		std::string fragment_text = "";
-		std::string vertext_text = "";
-		std::string geometry_text = "";
 	};
-
 
 	// This class is exported from the MathLibrary.dll  
 	
-	enum ShaderType {
-		fragment,
-		geometry,
-		vertex,
-	};
 
 	class Functions
 	{
 	public:
 
-		static SHADERS_API Shader CreateShader(char* name);
+		
+		 SHADERS_API ShaderProgram* CreateShader(char* name);
 
-		static SHADERS_API uint CompileShader(std::string path, ShaderType type);
+		SHADERS_API uint CompileShader(std::string path, ShaderType type);
 		// Returns a + b  
-		static SHADERS_API double Add(double a, double b);
+		 SHADERS_API void AddShaderList(Shader* newShader);
+		
 
-		// Returns a * b  
-		static SHADERS_API double Multiply(double a, double b);
-
-		// Returns a + (a * b)  
-		static SHADERS_API double AddMultiply(double a, double b);
+	private:
+		 std::list<Shader*> shaders;
+		 std::list<ShaderProgram*> programs;
 	};
 
+
+	
 
 
 }
